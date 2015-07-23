f1678d8a01f7b5d7129e16d265f92d7c215209b6
unknown <D:\Program Files (x86)\The Bat!\Mail>
2015-07-23 11:57:19 +0200

    screenshot

--- app.js
+++ app.js
@@ -1,29 +1,126 @@
 var exec = require('child_process').execFile;
 var request = require('request');
-var debug = console.log;
-var checkedComments = [];
-var SEMAPHORE = false;
+var fs = require('fs');
 
+//constructor + class-vars
 var Grabber = function(globalArgs){
 	globalArgs = globalArgs || 'flags=1';
-	this.keys=[];
+	this.settingsFileName = Grabber.settingsFile;
 	this.config = {
 		'ahk-exe': 'D:\\Program Files\\AutoHotkey\\AutoHotkey.exe',
 		'ahk-script': (__dirname + '\\key.ahk'),
-		'repeat': 20000, //in ms
+		'repeat': Grabber.time*2, //in ms
 		'global-args': globalArgs,
-		'items-get-url':'find-out-yourself/api/items/get?',
-		'items-info-url':'find-out-yourself/api/items/info?itemId='
+		'items-get-url':'http://find-out-yourself/api/items/get?',
+		'items-info-url':'http://find-out-yourself/api/items/info?itemId='
 	};
+	this.initSettingsFile();
 	this.run();
 }
 
+//statics
+Grabber.time = 5000;
+Grabber.SEMAPHORE = false;
+Grabber.settingsFile = "settings.js";
+Grabber.keys=[];
+Grabber.checkedComments = [];
+//Grabber.debugFile = fs.openSync("debug.txt", "a+");
+//Grabber.keyFile = fs.openSync("keys.txt", "a+");
+
+//methods
+Grabber.debug = function(args){
+	/*
+	var str="";
+	for(var key in arguments){
+		str=str+arguments[key]+" ";
+	};
+	fs.writeSync(Grabber.debugFile, "\r\n"+str, Grabber.filewriteDebug);
+	*/
+	console.log.apply(this, arguments);
+};
+
+Grabber.filewriteDebug=function(err){
+	if(err){
+		console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		console.log("ERROR IN FILE WRITE!!!!", err);
+	}
+};
+
+Grabber.prototype.readSettingsFile = function(){
+	var self = this;
+	fs.exists(this.settingsFileName, function(exists){
+		if(exists){
+			var settings = JSON.parse(fs.readFileSync(self.settingsFileName, 'utf8'));
+			if(settings.keys) Grabber.keys = settings.keys;
+			if(settings.checkedComments) Grabber.checkedComments = settings.checkedComments;
+		}
+	});
+}
+Grabber.prototype.writeSettingsFile = function(){
+	var settings = {
+		'keys': Grabber.keys,
+		'checkedComments': Grabber.checkedComments
+	};
+	fs.writeFileSync(this.settingsFileName, JSON.stringify(settings), 'utf8');
+}
+
+Grabber.prototype.initSettingsFile = function(){
+	var self = this;
+
+	this.readSettingsFile();
+
+	var saveSettings = function(code) {
+		console.log('XXXX About to exit with code:', code);
+		self.writeSettingsFile();
+	}
+	var saveSettingsExit = function(code) {
+		saveSettings(code);
+		process.exit();
+	}
+	process.on('SIGINT', saveSettingsExit); // ctrl+c
+	process.on('exit', saveSettingsExit);
+	process.on('uncaughtException', saveSettingsExit);
+	process.on('SIGBREAK', saveSettings); // ctrl+break
+};
+
+Grabber.prototype.adjustTime = function(anzahlOPs){
+	var max=15000;
+	var min=4000;
+	var newTime=this.config['repeat'];
+
+	if(anzahlOPs<1){
+		newTime = newTime + 1000;
+	}
+	if(anzahlOPs>2){
+		newTime = newTime - 2000; 
+	}
+	if(anzahlOPs>4){
+		newTime = newTime - 5000; 
+	}
+
+	if(newTime>max){
+		newTime = max;
+	}
+	if(newTime<min){
+		newTime = min;
+	}
+	/*
+	if(this.config['repeat']!=newTime){
+		Grabber.debug("+++adjusted time:", newTime, "got:", anzahlOPs);
+	}else{
+		Grabber.debug("+++not-adjusted time:", newTime, "got:", anzahlOPs);
+	}
+	*/
+	this.config['repeat']=newTime;
+}
+
 Grabber.prototype.execAHK = function(key){
+	var self = this;
 	/* 
-	STOP! SINGLE-RUN-FUNCTION! no multiple runs! its running if SEMAPHORE == true
+	STOP! SINGLE-RUN-FUNCTION! no multiple runs! its running if Grabber.SEMAPHORE == true
 	to avoid multirun, all calls are sheduling themself again in a second and terminate.
 	*/
-	if(SEMAPHORE){
+	if(Grabber.SEMAPHORE){
 		setTimeout((function(self){
 			return function(){
 				self.execAHK(key);
@@ -31,36 +128,36 @@ Grabber.prototype.execAHK = function(key){
 		})(this), 1000);
 		return;
 	};
-	SEMAPHORE=true;
+	Grabber.SEMAPHORE=true;
 
 	var executeFile = this.config['ahk-exe'];
 	var executeArgs = [this.config['ahk-script'], key];
 
 	exec(executeFile, executeArgs, function(err, data) {
-		SEMAPHORE=false;
+		Grabber.SEMAPHORE=false;
 		if(err)
-			debug("executeArgs, err, data:", executeArgs, err, data);
+			Grabber.debug("executeArgs, err, data:", executeArgs, err, data);
 	});
 };
 
 Grabber.prototype.run = function() {
-	var that = this;
-	this.keys.forEach(function(key){
-		that.execAHK(key);
+	var self = this;
+
+	//fs.writeSync(Grabber.keyFile, "\r\n==== neuer Lauf: ====\r\n", Grabber.filewriteDebug);
+
+	Grabber.keys.forEach(function(key){
+		//self.execAHK(key);
+		Grabber.debug("has key (from com/pic",key.comID,"/",key.picID,"):", key.key);
 	});
+
 	this.loop();	
-	setInterval((function(self){
-		return function(){
-			self.loop();
-		}
-	})(this), this.config['repeat']);
 };
 
 Grabber.prototype.loop = function(){
-	var that = this;
-	request((that.config['items-get-url'])+(that.config['global-args']), function (error, response, body) {
+	var self = this;
+	request((self.config['items-get-url'])+(self.config['global-args']), function (error, response, body) {
 		if (error || response.statusCode != 200) {
-			debug(error);
+			Grabber.debug(error);
 			return;
 		}
 
@@ -68,7 +165,7 @@ Grabber.prototype.loop = function(){
 		var tested = 0;
 
 		var list=JSON.parse(body);
-		debug("got "+list.items.length+" OPs ("+that.config['global-args']+")");
+		Grabber.debug("got "+list.items.length+" OPs ("+self.config['global-args']+")"+" timing: "+self.config['repeat']);
 		list.items.forEach(function(listitem){
 			pictureIds.push(listitem.id);
 		});
@@ -81,8 +178,7 @@ Grabber.prototype.loop = function(){
 			}
 			first--;
 
-			request((that.config['items-info-url'])+pictureId, function (error, response, body) {
-				tested++;
+			request((self.config['items-info-url'])+pictureId, function (error, response, body) {
 				if (error || response.statusCode != 200) {
 					debug(error);
 					return;
@@ -90,28 +186,45 @@ Grabber.prototype.loop = function(){
 
 				//console.log(body) // Print the body of response.
 				var picturedata=JSON.parse(body);
-				//debug("picturedata", picturedata);
+				//Grabber.debug("picturedata", picturedata);
 				picturedata.comments.forEach(function(comment){
-					//debug("got comment", comment.content);
-					//debugl(".");
-					if(checkedComments.indexOf(comment.id)<0){
-						checkedComments.push(comment.id);
-						debug("new (comID, PicID):", comment.id, pictureId, comment.content);
-						var foundKeys = comment.content.match(/\s[\w\d]{5}\-[\w\d]{5}\-[\w\d]{5}(?=\s)/);
+					//Grabber.debug("got comment", comment.content);
+					//Grabber.debugl(".");
+					if(Grabber.checkedComments.indexOf(comment.id)<0){
+						tested++;
+						Grabber.checkedComments.push(comment.id);
+						Grabber.debug("  new (comID, PicID):", comment.id, pictureId, comment.content);
+						var foundKeys = comment.content.match(/(\s|^)[\w\d]{5}\-[\w\d]{5}\-[\w\d]{5}(?=(\s|$))/g);
 						if(foundKeys != null){
 							foundKeys.forEach(function(key){
-								that.keys.push(key);
-								debug(key);
-								that.execAHK(key);
+								key = key.match(/[^\s]+/)[0];
+								Grabber.keys.push({'key':key, 'picID':pictureId, 'comID':comment.id});
+								Grabber.debug("=================================");
+								Grabber.debug(key);
+								Grabber.debug("=================================");
+								//fs.writeSync(Grabber.keyFile, "ComID/PicID: "+comment.id+"/"+pictureId+" | "+key+"\r\n", Grabber.filewriteDebug)
+								self.execAHK(key);
 							});
 						}
 					}
 				});
 			})
 		});
-	})
+		setTimeout((function(self){
+			return function(){
+				try{
+					self.adjustTime(tested);
+				}catch(e){};
+				setTimeout((function(self){
+					return function(){
+						self.loop();
+					}
+				})(self), self.config['repeat']);
+			}
+		})(self), 500);
+	});
 };
 
 new Grabber();
-setTimeout(function(){new Grabber('flags=1&promoted=1');}, 10000);
+setTimeout(function(){new Grabber('flags=1&promoted=1');}, Grabber.time);
 
--- key.ahk
+++ key.ahk
@@ -1,5 +1,6 @@
 ﻿#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
-#Warn  ; Enable warnings to assist with detecting common errors.
+; #Warn  ; Enable warnings to assist with detecting common errors.
+#include, Gdip_All.ahk 
 SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
 SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
 CoordMode, Mouse, Window
@@ -10,6 +11,7 @@ SteamExe := "D:\Program Files (x86)\Steam\Steam.exe"
 SteamArg := "steam://open/activateproduct"
 
 SteamCmd := SteamExe . " " . SteamArg
+ImgFile := "screen_" . A_Now . ".png"
 
 Run, %SteamCmd%
 WinWaitActive, Product Activation
@@ -17,9 +19,27 @@ WinWaitActive, Product Activation
 Send {Enter}{Enter}
 Sleep, 200
 Send %Key%
-Sleep, 1000
-Send {Enter}
-Sleep, 7000
-Send {Enter}{Enter}{Enter}
-Sleep, 12000
-Send {Enter}
+Sleep, 200
+Send {Enter} ;Next
+Sleep, 200
+WinWaitActive, Product Activation
+Sleep, 200
+Screenshot(ImgFile)
+; Duplicate -> schon geclaimt! macht nix, enter+esc schließen das fenster
+Send {Enter}{Esc}
+
+return
+
+
+
+Screenshot(outfile)
+{
+    pToken := Gdip_Startup()
+
+    screen=0|0|%A_ScreenWidth%|%A_ScreenHeight%
+    pBitmap := Gdip_BitmapFromScreen(screen)
+
+    Gdip_SaveBitmapToFile(pBitmap, outfile, 100)
+    Gdip_DisposeImage(pBitmap)
+    Gdip_Shutdown(pToken)
+}
